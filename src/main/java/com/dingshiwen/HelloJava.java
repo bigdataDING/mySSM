d75f56af1819aa0e8bcff5f319a061708ed4eca6
dingshiwen <1156912597@qq.com>
2022-08-13 15:08:07 +0800

    first commit

---
+++ src/main/java/com/dingshiwen/HelloJava.java
@@ -0,0 +1,11 @@
+package com.dingshiwen;
+
+/**
+ * @author dingshiwen
+ * @date 2022/8/13 - 15:04
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
